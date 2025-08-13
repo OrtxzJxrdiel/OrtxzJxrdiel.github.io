839fa64373f167334b0681e9292dc81f346c3d5e
OrtxzJxrdiel <elkin.ruiz1609@estu.unan.edu.ni>
2025-08-12 22:38:51 -0600

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+/*=============== COUNTDOWN ===============*/
+const launchDate = new Date("2026-08-12T00:00:00").getTime();
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const distance = launchDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((distance / (1000 * 60)) % 60);
+  const seconds = Math.floor((distance / 1000) % 60);
+
+  document.getElementById("days").textContent = String(days).padStart(2, "0");
+  document.getElementById("hours").textContent = String(hours).padStart(2, "0");
+  document.getElementById("minutes").textContent = String(minutes).padStart(
+    2,
+    "0"
+  );
+  document.getElementById("seconds").textContent = String(seconds).padStart(
+    2,
+    "0"
+  );
+}, 1000);
+
